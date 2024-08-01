c1fed149840b60498a941c8fe6b4102408d6dcbf
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2024-07-31 21:25:16 -0300

    update

--- src/main/java/com/jcode/inventory_control/entities/product/CategoryConverter.java
+++ src/main/java/com/jcode/inventory_control/entities/product/CategoryConverter.java
@@ -1,7 +1,6 @@
 package com.jcode.inventory_control.entities.product;
 
 import jakarta.persistence.AttributeConverter;
-import jakarta.persistence.Converter;
 
 public class CategoryConverter implements AttributeConverter<Category, String> {
 
