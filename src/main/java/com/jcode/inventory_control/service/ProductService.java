38c1de7a5c36e0a0a42520aac7c7161f52afd8ad
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2024-08-08 20:13:46 -0300

    update

---
+++ src/main/java/com/jcode/inventory_control/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.jcode.inventory_control.service;
+
+import com.jcode.inventory_control.entities.product.Product;
+import com.jcode.inventory_control.entities.product.ProductRequestDTO;
+import com.jcode.inventory_control.repositories.AddressRepository;
+import com.jcode.inventory_control.repositories.BarCodeRepository;
+import com.jcode.inventory_control.repositories.ProductAddressRepository;
+import com.jcode.inventory_control.repositories.ProductRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final BarCodeRepository barCodeRepository;
+    private final ProductRepository productRepository;
+    private final AddressRepository addressRepository;
+    private final ProductAddressRepository productAddressRepository;
+
+    public ProductService(
+            BarCodeRepository barCodeRepository,
+            ProductRepository productRepository,
+            AddressRepository addressRepository,
+            ProductAddressRepository productAddressRepository) {
+        this.barCodeRepository = barCodeRepository;
+        this.productRepository = productRepository;
+        this.addressRepository = addressRepository;
+        this.productAddressRepository = productAddressRepository;
+    }
+
+    public ResponseEntity<Product> saveProduct(ProductRequestDTO data) {
+
+        Product product = new Product();
+        product.setName(data.getName());
+        product.setDescription(data.getDescription());
+        product.setPrice(data.getPrice());
+        product.setImgUrl(data.getImgUrl());
+        product.setBlocked(false);
+        product.getCategory(data.getCategory());
+    }
+}
